42235b72892dda4887c515a17a73bad8a9f57970
김영채 <larmi9865@gmail.com>
2022-10-27 13:20:26 +0900

    2

--- 02.HTML학습/04.JS학습 (1)/JS응용구현/shoppingMall_PJ/js/main.js
+++ 02.HTML학습/04.JS학습 (1)/JS응용구현/shoppingMall_PJ/js/main.js
@@ -41,6 +41,11 @@ function loadFn() {
     // 1. 호출확인
     console.log("로딩완료!");
 
+    // 광클금지 함수
+    // 광클금지 상태변수
+    let prot = 0; // 0-허용, 1-불허용
+
+
     // 2. 대상선정
     // 2-1. 이벤트대상: .abtn
     const abtn = document.querySelectorAll(".abtn");
@@ -48,6 +53,11 @@ function loadFn() {
     const slide = document.querySelector("#slide");
 
     abtn[1].onclick = () => {
+        // 광클금지
+        if(prot) return; // 나가!
+        prot = 1; // 잠금!
+        setTimeout(()=>prot=0,800);
+
 
         slide.style.left = "-100%";
         slide.style.transition = ".8s";
@@ -61,5 +71,39 @@ function loadFn() {
         }, 800);
     }
 
-} //////////////// loadFn 함수 ///////////////
+    // 3-2. 왼쪽버튼 클릭시 : 왼쪽버튼 abtn변수 0번째
+    abtn[0].onclick = () => {
+        // 광클금지
+        if(prot) return; // 나가!
+        prot = 1; // 잠금!
+        setTimeout(()=>prot=0,800);
+
+        // 1. 맨뒤요소를 잘라서 맨앞으로 이동한다!
+        // 대상: slide변수 -> ul#slide
+        // 사용메서드: insertBefore(넣을놈,넣을놈전놈)
+        // 현재 li자식요소 수집하기!
+        let cli = slide.querySelectorAll("li");
+        slide.insertBefore(cli[cli.length-1],cli[0]);
+        // 2. 왼쪽 바깥에 -100% left값 주기!
+        slide.style.left = "-100%";
+        // 첫번째 실행후 생긴 트랜지션 없애기!
+        slide.style.transition = "none"
+        
+        // 2번과 3번 코드사이에 시차필요!
+        // setTimeout()사용!
+        // -> 시간을 0으로 해도 코드실행구역을 분리하므로
+        // 코드가 별도로 실행된다!(순서를 지킴)
+        setTimeout(()=>{
+            // 3. left 값을 0으로 트랜지션 주기
+            slide.style.left = "0";
+            slide.style.transition = ".8s"
+
+        },10)
+
+    };
+
+    // 4. 슬라이드가 이동되므로 순서를 알수 있는
+    // 고유순번용 속성을 만들어서 넣어준다!
+
+}; //////////////// loadFn 함수 ///////////////
 /////////////////////////////////////////////
